e14acb5888f543e003a263b2be2591b1792f23f4
Asheer Rizvi <asheer.vil@gmail.com>
2021-07-20 17:01:27 +0530

    Adding Auth to our Application

--- sick-fits/backend/schemas/User.ts
+++ sick-fits/backend/schemas/User.ts
@@ -1,13 +1,13 @@
-import { list } from "@keystone-next/keystone/schema";
-import { text, password, relationship } from "@keystone-next/fields";
+import { list } from '@keystone-next/keystone/schema';
+import { text, password, relationship } from '@keystone-next/fields';
 
 export const User = list({
   // access:
   // ui
   fields: {
     name: text({ isRequired: true }),
-    emai: text({ isRequired: true, isUnique: true }),
+    email: text({ isRequired: true, isUnique: true }),
     password: password(),
     //  TODO: add roles, cart and orders.
-  }
-})
+  },
+});
