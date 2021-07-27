8ca6748c94d67ba4289b12b8e38ca6a52560083b
Asheer Rizvi <asheer.vil@gmail.com>
2021-07-27 22:01:02 +0530

    A real good lesson in React Forms and Custom Hooks

---
+++ sick-fits/frontend/components/CreateProduct.js
@@ -0,0 +1,54 @@
+import useForm from '../lib/useForm';
+
+export default function CreateProduct() {
+  const { inputs, handleChange, clearForm, resetForm } = useForm({
+    name: 'hello there',
+    price: '1234',
+    description: 'what a nice price',
+  });
+
+  return (
+    <form>
+      <label htmlFor="name">
+        Name
+        <input
+          type="text"
+          id="name"
+          name="name"
+          placeholder="Name"
+          value={inputs.name}
+          onChange={handleChange}
+        />
+      </label>
+      <label htmlFor="price">
+        Price
+        <input
+          type="number"
+          id="price"
+          name="price"
+          placeholder="Price"
+          value={inputs.price}
+          onChange={handleChange}
+        />
+      </label>
+      <label htmlFor="description">
+        Description
+        <input
+          type="text"
+          id="description"
+          name="description"
+          placeholder="Description"
+          value={inputs.description}
+          onChange={handleChange}
+        />
+      </label>
+
+      <button onClick={clearForm} type="button">
+        Clear form
+      </button>
+      <button onClick={resetForm} type="button">
+        Reset form
+      </button>
+    </form>
+  );
+}
