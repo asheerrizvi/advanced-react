24c1c67607c6f941a577d0bb47196eb87f9c128f
Asheer Rizvi <asheer.vil@gmail.com>
2021-08-30 21:26:32 +0530

    Password Reset - sending the email

---
+++ sick-fits/frontend/components/Reset.js
@@ -0,0 +1,78 @@
+import gql from 'graphql-tag';
+import { useMutation } from '@apollo/client';
+import Form from './styles/Form';
+import useForm from '../lib/useForm';
+import DisplayError from './ErrorMessage';
+
+const RESET_MUTATION = gql`
+  mutation RESET_MUTATION(
+    $email: String!
+    $password: String!
+    $token: String!
+  ) {
+    redeemUserPasswordResetToken(
+      email: $email
+      password: $password
+      token: $token
+    ) {
+      code
+      message
+    }
+  }
+`;
+
+export default function Reset({ token }) {
+  const { inputs, handleChange, resetForm } = useForm({
+    email: '',
+    password: '',
+    token,
+  });
+  const [reset, { data, error, loading }] = useMutation(RESET_MUTATION, {
+    variables: inputs,
+  });
+  const successfulError = data?.redeemUserPasswordResetToken?.code
+    ? data?.redeemUserPasswordResetToken
+    : undefined;
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    // Send the email and password to the GraphQL API.
+    const res = await reset().catch(console.error);
+    resetForm();
+  }
+
+  return (
+    <Form method="POST" onSubmit={handleSubmit}>
+      <h2>Reset your password</h2>
+      <DisplayError error={error || successfulError} />
+      <fieldset>
+        {data?.redeemUserPasswordResetToken === null && (
+          <p>Success! You can now sign in!</p>
+        )}
+        <label htmlFor="email">
+          Email
+          <input
+            type="email"
+            name="email"
+            placeholder="Your email address"
+            autoComplete="email"
+            value={inputs.email}
+            onChange={handleChange}
+          />
+        </label>
+        <label htmlFor="password">
+          Password
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            autoComplete="password"
+            value={inputs.password}
+            onChange={handleChange}
+          />
+        </label>
+        <button type="submit">Request Reset</button>
+      </fieldset>
+    </Form>
+  );
+}
