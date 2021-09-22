53a0f34366e39b0faeff411c2154a7e0f4606667
Asheer Rizvi <asheer.vil@gmail.com>
2021-09-22 18:15:58 +0530

    Custom Checkout Mutation with Stripe

---
+++ sick-fits/backend/mutations/checkout.ts
@@ -0,0 +1,75 @@
+import { KeystoneContext } from '@keystone-next/types';
+import {
+  CartItemCreateInput,
+  OrderCreateInput,
+} from '../.keystone/schema-types';
+import stripeConfig from '../lib/Stripe';
+import { Session } from '../types';
+
+const graphql = String.raw;
+
+interface Arguments {
+  token: string;
+}
+
+async function checkout(
+  root: any,
+  { token }: Arguments,
+  context: KeystoneContext
+): Promise<OrderCreateInput> {
+  // 1. Make sure they are signed in.
+  const userId = context.session.itemId;
+  if (!userId) {
+    throw new Error('Sorry! You must be signed in to create an order.');
+  }
+  const user = await context.lists.User.findOne({
+    where: { id: userId },
+    resolveFields: graphql`
+      id
+      name
+      email
+      cart {
+        id
+        quantity
+        product {
+          id
+          name
+          price
+          description
+          photo {
+            id
+            image {
+              id
+              publicUrlTransformed
+            }
+          }
+        }
+      }
+    `,
+  });
+  console.dir(user, { depth: null });
+
+  // 2. Calculate the total price for their order.
+  const cartItems = user.cart.filter((cartItem) => cartItem.product);
+  const amount = cartItems.reduce(
+    (tally: number, cartItem: CartItemCreateInput) =>
+      tally + cartItem.quantity * cartItem.product.price,
+    0
+  );
+  // 3. Create the charge with the stripe library.
+  const charge = await stripeConfig.paymentIntents
+    .create({
+      amount,
+      currency: 'INR',
+      confirm: true,
+      payment_method: token,
+    })
+    .catch((err) => {
+      console.log(err);
+      throw new Error(err.message);
+    });
+  // 4. Convert the cart items to order items.
+  // 5. Create the order and return it.
+}
+
+export default checkout;
