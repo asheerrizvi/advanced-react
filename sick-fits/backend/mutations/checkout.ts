590729672640321d78df31c956fdda7a5cb6b02c
Asheer Rizvi <asheer.vil@gmail.com>
2021-09-23 18:00:29 +0530

    Creating our Order and OrderItems in our Mutation

--- sick-fits/backend/mutations/checkout.ts
+++ sick-fits/backend/mutations/checkout.ts
@@ -69,9 +69,32 @@ async function checkout(
       throw new Error(err.message);
     });
   console.log(charge);
-
   // 4. Convert the cart items to order items.
+  const orderItems = cartItems.map((cartItem) => {
+    const orderItem = {
+      name: cartItem.product.name,
+      description: cartItem.product.description,
+      price: cartItem.product.price,
+      quantity: cartItem.quantity,
+      photo: { connect: { id: cartItem.product.photo.id } },
+    };
+
+    return orderItem;
+  });
   // 5. Create the order and return it.
+  const order = await context.lists.Order.createOne({
+    data: {
+      total: charge.amount,
+      charge: charge.id,
+      items: { create: orderItems },
+    },
+  });
+  // 6. Clean up any old cart items.
+  const cartItemsIds = cartItems.map((cartItem) => cartItem.id);
+  await context.lists.CartItem.deleteMany({
+    ids: cartItemsIds,
+  });
+  return order;
 }
 
 export default checkout;
