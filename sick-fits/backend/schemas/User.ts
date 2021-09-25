e0525ee4fd83ce9cc2b916fe31145e5d47c21a4a
Asheer Rizvi <asheer.vil@gmail.com>
2021-09-25 18:19:06 +0530

    Creating the roles and permission schema + UI

--- sick-fits/backend/schemas/User.ts
+++ sick-fits/backend/schemas/User.ts
@@ -16,7 +16,10 @@ export const User = list({
         itemView: { fieldMode: 'read' },
       },
     }),
-    //  TODO: add roles.
     orders: relationship({ ref: 'Order.user', many: true }),
+    role: relationship({
+      ref: 'Role.assignedTo',
+      // TODO: Add access control
+    }),
   },
 });
