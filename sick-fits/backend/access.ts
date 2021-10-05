6c60f6b33d8900a6d785c414a5256c75f433a844
Asheer Rizvi <asheer.vil@gmail.com>
2021-10-05 17:36:14 +0530

    More flexible rule based functions.

--- sick-fits/backend/access.ts
+++ sick-fits/backend/access.ts
@@ -23,3 +23,22 @@ export const permissions = {
 };
 
 // Rule based functions.
+// Rules can return a Boolean - yes or no or a filter which limits
+// which products they can CRUD.
+export const rules = {
+  canManageProducts({ session }: ListAccessArgs) {
+    // 1. Do they have the permission of canManageProducts
+    if (permissions.canManageProducts({ session })) {
+      return true;
+    }
+    // 2. If not do they own this item?
+    return { user: { id: session.itemId } };
+  },
+  canReadProducts({ session }: ListAccessArgs) {
+    if (permissions.canManageProducts({ session })) {
+      return true; // They can read everything!
+    }
+    // THey should only see available products (based on the status fields)
+    return { status: 'AVAILABLE' };
+  },
+};
